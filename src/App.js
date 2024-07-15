52ff3df05e9978df805e6ea47a167b87dc8fad00
ashutosh786kr <ashuronnie1234@gmail.com>
2024-07-15 11:13:50 +0530

    here are the changes

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 // eslint-disable-next-line no-unused-vars
-import user from './components/user'; // Corrected import path and component name
-import ServiceProvider from './components/serviceProvider'; // Corrected import path and component name
-import Shop from './components/shop'; // Corrected import path and component name
-import Order from './components/order'; // Corrected import path and component name
-import Tracking from './components/tracking'; // Corrected import path and component name
-import Item from './components/item'; // Corrected import path and component name
-import CartItem from './components/cartItem'; // Corrected import path and component name
+import user from "./component/user"; // Corrected import path and component name
+import ServiceProvider from "./component/serviceProviders"; // Corrected import path and component name
+import Shop from "./component/shop"; // Corrected import path and component name
+import Order from "./component/order"; // Corrected import path and component name
+import Tracking from "./component/tracking"; // Corrected import path and component name
+import Item from "./component/item"; // Corrected import path and component name
+import CartItem from "./component/cartItem"; // Corrected import path and component name
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img
+          src={
+            "https://cdn.pixabay.com/photo/2024/01/28/16/17/ai-generated-8537858_1280.png"
+          }
+          className="App-logo"
+          alt="logo"
+        />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -22,8 +27,7 @@ function App() {
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
-          rel="noopener noreferrer"
-        >
+          rel="noopener noreferrer">
           Learn React
         </a>
       </header>
